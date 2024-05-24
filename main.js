0d89687ac40e8979492f2738c9d8611c3c7be45c
alber@AlbertXS <albertxicola04@hotmail.com>
2024-05-24 22:00:59 +0200

    Index mejora

--- main.js
+++ main.js
@@ -31,3 +31,62 @@ document.addEventListener('DOMContentLoaded', function() {
     loadThemePreference();
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.getElementById('searchButton');
+    const searchInput = document.getElementById('searchInput');
+
+    searchButton.addEventListener('click', () => {
+        performSearch();
+    });
+
+    searchInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            performSearch();
+        }
+    });
+
+    function performSearch() {
+        const query = searchInput.value.toLowerCase();
+        if (query) {
+            clearHighlights();
+            const found = searchAndHighlight(query);
+            if (!found) {
+                alert('No se encontraron resultados para: ' + query);
+            }
+        }
+    }
+
+    function searchAndHighlight(query) {
+        const contentSections = document.querySelectorAll('main section');
+        let found = false;
+
+        contentSections.forEach(section => {
+            const sectionHTML = section.innerHTML;
+            const regex = new RegExp(`(${query})`, 'gi');
+            if (regex.test(sectionHTML)) {
+                found = true;
+                const newHTML = sectionHTML.replace(regex, '<mark class="highlight">$1</mark>');
+                section.innerHTML = newHTML;
+                if (!section.classList.contains('highlighted')) {
+                    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                    section.classList.add('highlighted');
+                }
+            }
+        });
+
+        return found;
+    }
+
+    function clearHighlights() {
+        const marks = document.querySelectorAll('mark.highlight');
+        marks.forEach(mark => {
+            const parent = mark.parentNode;
+            parent.replaceChild(document.createTextNode(mark.textContent), mark);
+            parent.normalize();
+        });
+        document.querySelectorAll('.highlighted').forEach(section => {
+            section.classList.remove('highlighted');
+        });
+    }
+});
